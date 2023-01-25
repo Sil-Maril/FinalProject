5e52a66e2048895f46c084fc405f84970ddb1131
Sil-Maril <mariamputkaradze@gmail.com>
2023-01-26 00:03:34 +0400

    update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
-
 var toggleButton = document.querySelector('.toggle-menu');
 var navBar = document.querySelector('.nav-bar');
 
 // function to toggle the menu when the button is clicked
 function toggleMenu() {
+    navBar.classList.toggle('fixed');
     navBar.classList.toggle('toggle');
 }
 
-//Event listener for the toggle button
-toggleButton.addEventListener('click', function(){
-   toggleMenu();
-   var section = document.querySelector('.section');
-   section.scrollIntoView({ behavior: 'smooth' });
-});
-
-$('.toggle-menu').click(function(){
-  $('.nav-bar').toggle();
-  $('html, body').animate({
-      scrollTop: $(".section").offset().top
-  }, 2000);
-});
+// Event listener for the toggle button
+toggleButton.addEventListener('click', toggleMenu);
